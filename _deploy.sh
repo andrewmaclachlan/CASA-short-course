27cdd220e80dcede1e9e6ee157150f7076eec046
andrewmaclachlan <51865303+andrewmaclachlan@users.noreply.github.com>
2020-02-21 14:43:57 +0000

    git config

--- _deploy.sh
+++ _deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git config --global user.email "a.maclachlan@ucl.ac.uk"
-git config --global user.name "andrewmaclachlan"
+#git config --global user.email "a.maclachlan@ucl.ac.uk"
+#git config --global user.name "andrewmaclachlan"
 
 # clone the repository to the book-output directory
 git clone -b gh-pages \
