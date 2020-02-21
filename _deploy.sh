ba8df70adb7ca7cbcaff4b0830f196c6688ed59a
andrewmaclachlan <51865303+andrewmaclachlan@users.noreply.github.com>
2020-02-21 14:52:03 +0000

    remove deplpy

--- _deploy.sh
+++ _deploy.sh
@@ -4,12 +4,12 @@
 #git config --global user.name "andrewmaclachlan"
 
 # clone the repository to the book-output directory
-git clone -b gh-pages \
-  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
-  book-output
-cd book-output
-git rm -rf *
-cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book"
-git push -q origin gh-pages
+#git clone -b gh-pages \
+#  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+#  book-output
+#cd book-output
+#git rm -rf *
+#cp -r ../_book/* ./
+#git add --all *
+#git commit -m"Update the book"
+#git push -q origin gh-pages
